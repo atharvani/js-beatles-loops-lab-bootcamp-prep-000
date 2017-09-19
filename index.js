4a65a3c74912e6c3afa2aa99075c3ef6f18c3851
Shital Sanap <shital.sanap@gmail.com>
2017-09-19 22:21:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,16 +21,19 @@ function iLoveTheBeatles(number)
 {
   var array1 = new Array;
   var i =0;
+  if(number === 7)
+  {
   do{
       array1 = [...array1, 'I love the Beatles!'];
       i++;
   }while(i<number);
-
+}
+else if (number === 17)
+{
   var array2 = new Array;
- i =0;
-  while(i<number){
+
       array2.push("I love the Beatles!");
-      i++;
+      
   }
   return array1, array2;
 }
